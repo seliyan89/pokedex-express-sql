5348a6795942c369b69267f31c9538a7a3e1a3bc
Seliyan Silvarajoo <seliyansilvarajoo@Seliyans-MacBook-Pro.local>
2018-03-28 08:58:56 +0800

    WIP

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ const { Client } = require('pg');
 
 // Initialise postgres client
 const client = new Client({
-  user: 'akira',
+  user: 'seliyansilvarajoo',
   host: '127.0.0.1',
   database: 'pokemons',
   port: 5432,
 });
 
+// client.connect((err) => {
+//   if (err) console.error('connection error:', err.stack);
+// });
 /**
  * ===================================
  * Configurations and set up
@@ -36,17 +39,57 @@ app.set('view engine', 'handlebars');
  * ===================================
  */
 
-app.get('/', (req, res) => {
-  // query database for all pokemon
-
-  // respond with HTML page displaying all pokemon
-});
+function retrieveAllPokemons(res){
+  let queryText="select * from pokemon";
+  client.query(queryText,(err,response)=>{
+    let pokemonList=response.rows;
+    let context={pokemonList:pokemonList};
+    res.render('home',context);
+    // client.end();
+  });
+};
 
 app.get('/new', (request, response) => {
   // respond with HTML page with form to create new pokemon
   response.render('new');
 });
 
+app.get('/:id',(req,res)=>{
+    let pokemonQuery=req.params.id;
+    let queryText="select * from pokemon where id="+pokemonQuery;
+    client.connect();
+    client.query(queryText,(err,dbResp)=>{
+      let pokemon=dbResp.rows;
+      let context={pokemon:pokemon[0]};
+      res.render('edit',context);
+      // client.end();
+    })
+});
+
+app.get('/:id/edit',(req,res)=>{
+  // client.end();
+});
+
+app.post('/',(req,res)=>{
+  let newPokemon = req.body;
+  client.connect();
+  let values=[];
+  values.push(newPokemon.num);
+  values.push(newPokemon.name);
+  values.push(newPokemon.img);
+  values.push(newPokemon.height);
+  values.push(newPokemon.weight);
+  let queryText = "insert into pokemon (num,name,img,height,weight) values($1,$2,$3,$4,$5)";
+  client.query(queryText,values,(err,response)=>{
+    retrieveAllPokemons(res);
+  });
+});
+
+app.get('/', (req, res) => {
+  client.connect();
+  let queryText="select * from pokemon";
+  retrieveAllPokemons(res);
+});
 
 app.post('/pokemon', (req, response) => {
   let params = req.body;
@@ -66,11 +109,27 @@ app.post('/pokemon', (req, response) => {
         // redirect to home page
         response.redirect('/');
       }
-      client.end();
     });
   });
 });
 
+// function retrieveOnePokemon(pokemonQuery,res){
+//   var createQuery = new Promise(function(resolve,reject){
+//     let queryText="select * from pokemon where id="+pokemonQuery;
+//     // console.log(queryText);
+//     client.connect();
+//     resolve(queryText);
+//   });
+//   createQuery.then((queryText)=>{
+//     client.query(queryText)
+//       .then((result)=>{
+//         let pokemon=result.rows;
+//         let context={pokemon:pokemon[0]};
+//         res.render('edit',context);
+//         client.end();
+//       });
+//     });
+// };
 
 /**
  * ===================================
